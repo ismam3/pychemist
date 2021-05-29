a4fbb73d5b5025c0d36c099e42a2a8e9a9ed16de
ismam3 <ismamlabib3@gmail.com>
2021-05-30 01:20:53 +0600

    change routing url in docs

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/pychemist" component={Home}></Route>
         </Switch>
       </BrowserRouter>
     </div>
